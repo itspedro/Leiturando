a2cf61a3b83335293aab4208b02fb100bdda3e7a
Pedro Reis <itspdro@outlook.com>
2023-11-29 14:03:16 -0300

    [+]chore: update UI elements.

--- lib/src/pages/Home/home_view.dart
+++ lib/src/pages/Home/home_view.dart
@@ -28,103 +28,107 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-          length: myTabs.length,
-          child: Scaffold(
-            appBar: AppBar(
-              title: const Text('Leiturando'),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () {
-                    Navigator.restorablePushNamed(
-                        context, SettingsView.routeName);
-                  },
-                ),
-              ],
-              bottom: const TabBar(
-                tabs: myTabs,
-              ),
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Leiturando'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                Navigator.restorablePushNamed(context, SettingsView.routeName);
+              },
             ),
-            body: TabBarView (
-              children: [
-                buildTabView(context, items, false),
-                buildTabView(context, items, true),
-              ],
-            ),
+          ],
+          bottom: const TabBar(
+            tabs: myTabs,
           ),
-        );
+        ),
+        body: TabBarView(
+          children: [
+            buildTabView(context, items, false),
+            buildTabView(context, items, true),
+          ],
+        ),
+      ),
+    );
   }
 }
 
-
-Widget buildTabView(BuildContext context, List<Book> items, bool isFavoritesTab) {
+Widget buildTabView(
+    BuildContext context, List<Book> items, bool isFavoritesTab) {
   return ValueListenableBuilder(
     valueListenable: Hive.box('favorites').listenable(),
     builder: (context, Box box, child) {
-      final List<Book> books = isFavoritesTab 
-        ? items.where((item) => box.containsKey(item.id)).toList()
-        : items;
+      final List<Book> books = isFavoritesTab
+          ? items.where((item) => box.containsKey(item.id)).toList()
+          : items;
 
-        return GridView.builder(
-              padding: const EdgeInsets.all(20),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              restorationId: isFavoritesTab ? 'favoritesView' : 'homeView',
-              itemCount: books.length,
-              itemBuilder: (BuildContext context, int index) {
-                final Book book = books[index];
-                final bool isBookMarked = box.containsKey(book.id);
+      return GridView.builder(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          childAspectRatio: 0.7,
+          mainAxisSpacing: 10,
+        ),
+        restorationId: isFavoritesTab ? 'favoritesView' : 'homeView',
+        itemCount: books.length,
+        itemBuilder: (BuildContext context, int index) {
+          final Book book = books[index];
+          final bool isBookMarked = box.containsKey(book.id);
 
-                return GridTile(
-                  header: GridTileBar(
-                    trailing: IconButton(
-                      icon: Icon(
-                        isBookMarked ? Icons.bookmark : Icons.bookmark_border,
-                      ),
-                      onPressed: () async {
-                        if (isBookMarked) {
-                          await box.delete(book.id);
-                        } else {
-                          await box.put(book.id, book.id);
-                        }                     ScaffoldMessenger.of(context).showSnackBar(
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: GridTile(
+              header: GridTileBar(
+                title: const Text(""),
+                trailing: IconButton(
+                    icon: Icon(
+                      isBookMarked ? Icons.bookmark : Icons.bookmark_border,
+                      color: isBookMarked ? Colors.red : Colors.black,
+                      size: 30,
+                    ),
+                    onPressed: () async {
+                      if (isBookMarked) {
+                        await box.delete(book.id);
+                      } else {
+                        await box.put(book.id, book.id);
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
                         SnackBar(
                           content: Text(
                             !isBookMarked
-                              ? 'Adicionado aos favoritos'
-                              : 'Removido dos favoritos',
+                                ? 'Adicionado aos favoritos'
+                                : 'Removido dos favoritos',
                           ),
-                          duration: const Duration(milliseconds: 500),
+                          duration: const Duration(milliseconds: 1000),
                         ),
                       );
-                      },
-                    ),
+                    },
                   ),
-                  footer: GridTileBar(
-                    title: Text(book.title),
-                    subtitle: Text(book.author),
-                    backgroundColor: Colors.black45,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.restorablePushNamed(
-                          context,
-                          BookDetailsView.routeName,
-                        );
-                      },
-                      child: Image.asset(
-                        book.cover,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
+              ),
+              footer: GridTileBar(
+                title: Text(book.title),
+                subtitle: Text(book.author),
+                backgroundColor: Colors.black45,
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.restorablePushNamed(
+                    context,
+                    BookDetailsView.routeName,
+                  );
+                },
+                child: Image.asset(
+                  book.cover,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+        },
+      );
     },
   );
 }
