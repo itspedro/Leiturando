5caa427a971ab9cec170f1d19b542d20015a4608
Pedro Reis <itspdro@outlook.com>
2023-11-30 10:05:52 -0300

    [+]chore: update books type.

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -4,13 +4,15 @@ class Book {
   int id;
   String title;
   String author;
-  String cover;
+  String cover_url;
+  String download_url;
 
   Book({
     required this.id,
     required this.title,
     required this.author,
-    required this.cover,
+    required this.cover_url,
+    required this.download_url,
   });
 
 }
--- lib/repositories/books_repository.dart
+++ lib/repositories/books_repository.dart
@@ -6,19 +6,22 @@ class BooksRepository {
       id: 1,
       title: "Livro 1",
       author: "Autor 1",
-      cover: "assets/images/interstellar.jpg",
+      cover_url: "assets/images/interstellar.jpg",
+      download_url: "https://vocsyinfotech.in/envato/cc/flutter_ebook/uploads/22566_The-Racketeer---John-Grisham.epub",
     ),
     Book(
       id: 2,
       title: "Livro 2",
       author: "Autor 2",
-      cover: "assets/images/interstellar.jpg",
+      cover_url: "assets/images/interstellar.jpg",
+      download_url: "https://vocsyinfotech.in/envato/cc/flutter_ebook/uploads/22566_The-Racketeer---John-Grisham.epub",
     ),
     Book(
       id: 3,
       title: "Livro 3",
       author: "Autor 3",
-      cover: "assets/images/interstellar.jpg",
+      cover_url: "assets/images/interstellar.jpg",
+      download_url: "https://vocsyinfotech.in/envato/cc/flutter_ebook/uploads/22566_The-Racketeer---John-Grisham.epub",
     ),
   ];
 }
